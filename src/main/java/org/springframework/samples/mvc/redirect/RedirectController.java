bf0135b1fd071b814667d20ef05ffbac6a4bfaa6
aliakh <liakh.aliaksandr@gmail.com>
2019-06-25 07:55:23 +0300

    Update dependencies to latest version

--- src/main/java/org/springframework/samples/mvc/redirect/RedirectController.java
+++ src/main/java/org/springframework/samples/mvc/redirect/RedirectController.java
@@ -2,8 +2,6 @@ package org.springframework.samples.mvc.redirect;
 
 import javax.inject.Inject;
 
-import org.joda.time.LocalDate;
-
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("/redirect")
@@ -28,13 +28,13 @@ public class RedirectController {
 	@GetMapping("/uriTemplate")
 	public String uriTemplate(RedirectAttributes redirectAttrs) {
 		redirectAttrs.addAttribute("account", "a123");  // Used as URI template variable
-		redirectAttrs.addAttribute("date", new LocalDate(2011, 12, 31));  // Appended as a query parameter
+		redirectAttrs.addAttribute("date", LocalDate.of(2011, 12, 31));  // Appended as a query parameter
 		return "redirect:/redirect/{account}";
 	}
 
 	@GetMapping("/uriComponentsBuilder")
 	public String uriComponentsBuilder() {
-		String date = this.conversionService.convert(new LocalDate(2011, 12, 31), String.class);
+		String date = this.conversionService.convert(LocalDate.of(2011, 12, 31), String.class);
 		UriComponents redirectUri = UriComponentsBuilder.fromPath("/redirect/{account}").queryParam("date", date)
 				.build().expand("a123").encode();
 		return "redirect:" + redirectUri.toUriString();
